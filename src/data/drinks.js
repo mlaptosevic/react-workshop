a24d24ebe9183c0ea1cca41a8bf7b5766e8b9e3b
Laptosevic, Milenko <Milenko.Laptosevic@zuehlke.com>
2019-11-22 04:59:29 +0100

    Update drinks json

--- src/data/drinks.js
+++ src/data/drinks.js
@@ -2,17 +2,26 @@ const drinks = [
     {
         id: 1,
         name: 'Coca-cola',
-        url: ''
+        price: 43,
+        url: 'https://d3el976p2k4mvu.cloudfront.net/medias/sys_master/h4f/h50/8856190320670.png?buildNumber=1dfc5eade4925cd2e912e8a751ec4131207a4518'
     },
     {
         id: 2,
         name: 'Pepsi',
-        url: ''
+        price: 49,
+        url: 'https://d3el976p2k4mvu.cloudfront.net/medias/sys_master/h76/he5/8867155378206.jpg?buildNumber=1dfc5eade4925cd2e912e8a751ec4131207a4518'
     },
     {
         id: 3,
         name: 'Fanta',
-        url: ''
+        price: 54,
+        url: 'https://d3el976p2k4mvu.cloudfront.net/medias/sys_master/h70/he6/8844335939614.png?buildNumber=1dfc5eade4925cd2e912e8a751ec4131207a4518'
+    },
+    {
+        id: 4,
+        name: 'Sprite',
+        price: 54,
+        url: 'https://d3el976p2k4mvu.cloudfront.net/medias/sys_master/h0f/h24/8850344542238.png?buildNumber=1dfc5eade4925cd2e912e8a751ec4131207a4518'
     }
 ];
 
